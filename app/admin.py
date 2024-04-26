d3ca3fc62868be2444533ef09b5a775cd21cb56b
VarshaShaheen <varshashaheen2003@gmail.com>
2024-04-26 09:11:17 +0530

    created app

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'email', 'phone_number', 'company_incorporation_certificate_number', 'issue_date',
+                    'expiry_date', 'financial_year', 'date_of_submission']
+    search_fields = ['company_incorporation_certificate_number','company_name']
