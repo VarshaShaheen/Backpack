7e11328218b6087dbb113320232e91f3d53f42e9
VarshaShaheen <varshashaheen2003@gmail.com>
2024-06-11 20:23:25 +0530

    db changed

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Company
+from .models import CompanyDetail
 
 
-@admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ['company_name', 'email', 'phone_number', 'company_incorporation_certificate_number', 'issue_date',
-                    'expiry_date', 'financial_year', 'date_of_submission']
-    search_fields = ['company_incorporation_certificate_number','company_name']
+class CompanyDetailAdmin(admin.ModelAdmin):
+    list_display = [
+        'company_name_en', 'company_name_ar', 'email', 'mobile', 'website',
+        'national_address', 'po_box_no', 'city', 'country', 'contact_person_name',
+        'title', 'contact_person_mobile', 'fiscal_year_end', 'due_date_of_tax_fs_submission',
+        'vat_submission_period', 'latest_vat_filed', 'client_type', 'contract_start_date', 'contract_end_date'
+    ]
+    search_fields = ['company_name_en', 'company_name_ar', 'city', 'country', 'contact_person_name']
+    list_filter = ['city', 'country', 'client_type', 'fiscal_year_end', 'due_date_of_tax_fs_submission']
+
+
+admin.site.register(CompanyDetail, CompanyDetailAdmin)
--- app/models.py
+++ app/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 
 
-class Company(models.Model):
-    company_name = models.CharField(max_length=255)
+class CompanyDetail(models.Model):
+    company_name_en = models.CharField(max_length=255)
+    company_name_ar = models.CharField(max_length=255)
+    telephone = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20)
     email = models.EmailField()
-    phone_number = models.CharField(max_length=20)
-    company_incorporation_certificate_number = models.CharField(max_length=100)
-    issue_date = models.DateField()
-    expiry_date = models.DateField()
-    financial_year = models.DateField()
-    date_of_submission = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    website = models.URLField()
+    national_address = models.CharField(max_length=255)
+    po_box_no = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    contact_person_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    contact_person_mobile = models.CharField(max_length=20)
+    contact_person_email = models.EmailField()
+    fiscal_year_end = models.DateField()
+    due_date_of_tax_fs_submission = models.DateField()
+    vat_submission_period = models.CharField(max_length=100)
+    latest_vat_filed = models.DateField()
+    client_type = models.CharField(max_length=100)
+    contract_start_date = models.DateField()
+    contract_end_date = models.DateField()
 
     def __str__(self):
-        return self.company_name
+        return self.company_name_en
